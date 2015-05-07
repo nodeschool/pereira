dadf5e2c6a98df18a3b8ee4c61d3024b6a7d073b
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-05-06 21:57:45 -0500

    Testing map controls

--- js/grayscale.js
+++ js/grayscale.js
@@ -45,8 +45,8 @@ function init() {
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+        scrollwheel: true,
+        draggable: true,
 
         // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
