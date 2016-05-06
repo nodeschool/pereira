6362f87ec7558fd62392e16167ede76626ed1211
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-05-06 10:42:17 -0500

    actualizo info 2016. Closes #9

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(4.79067, -75.69016),
+        center: new google.maps.LatLng(4.808660, -75.704654),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(4.79067, -75.69016);
+    var myLatLng = new google.maps.LatLng(4.805574, -75.7255772);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
@@ -182,7 +182,7 @@ function init() {
                  '</div>' +
                  '<h3 id="firstHeading" class="firstHeading">NodeSchool Pereira</h3>'+
                  '<div id="bodyContent">'+
-                   '<p>El evento será en la sala 210 Bloque Y, de la Universidad Tecnológica de Pereira</p>' +
+                   '<p>El evento será en Universidad Católica de Pereira</p>' +
                  '</div>'+
                '</div>'
     });
